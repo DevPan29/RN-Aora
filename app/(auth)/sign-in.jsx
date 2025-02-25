caf1bb73d6c4902da05a8370d449bc82cd2fe1cf
AlePan <darenagel@hotmail.it>
2025-02-25 07:00:07 +0100

    added GloablProvider object

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,23 +1,46 @@
-import { Image, ScrollView, Text, View } from 'react-native'
+import { Alert, Image, ScrollView, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { getCurrentUser, signIn } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const SignIn = () => {
-  const [form, setform] = useState({
-    email: '',
-    password: ''
-  })
+  const { setUser, setIsLogged } = useGlobalContext();
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-  const [isSubmitting, setSubmitting] = useState(false)
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
 
-  const submit = () => {
+    setSubmitting(true);
 
-  }
+    try {
+      await signIn(form.email, form.password);
+      
+      const result = await getCurrentUser();
+      setUser(result);
+      
+      setIsLogged(true);
+      
+
+      Alert.alert("Success", "User signed in successfully");
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
 
   return (
@@ -33,7 +56,7 @@ const SignIn = () => {
           <FormField 
             title="Email"
             value={form.email}
-            handleChangeText={(e) => setform({ ...form,
+            handleChangeText={(e) => setForm({ ...form,
               email: e
              })}
              otherStyles="mt-7"
@@ -42,7 +65,7 @@ const SignIn = () => {
           <FormField 
             title="Password"
             value={form.password}
-            handleChangeText={(e) => setform({ ...form,
+            handleChangeText={(e) => setForm({ ...form,
               password: e
              })}
              otherStyles="mt-7"
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import { SplashScreen, Stack } from 'expo-router';
 import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
+import GlobalProvider from '../context/GlobalProvider'
 
 // this code prevents that the SplashScreen hide before the entire assets has been loaded
 SplashScreen.preventAutoHideAsync();
@@ -30,12 +31,14 @@ const RootLayout = () => {
   if (!fontsLoaded && !error) return null;
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} /> 
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> 
-     {/*  <Stack.Screen name="/search/[query]" options={{ headerShown: false }} />  */}
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} /> 
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> 
+      {/*  <Stack.Screen name="/search/[query]" options={{ headerShown: false }} />  */}
+      </Stack>
+    </GlobalProvider>
   )
 }
 
